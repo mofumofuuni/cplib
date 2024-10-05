850dc1173198c78e25c7e45d7fdc27005b6482b8
m0fu2 <141352755+mofumofuuni@users.noreply.github.com>
2024-10-05 20:52:57 +0900

    Delete document/Math/is_prime.hpp

--- document/Math/is_prime.hpp
+++
@@ -1,8 +0,0 @@
-// Prime Judge
-bool is_prime(long long n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (!(n & 1)) return false;
-    if (n < 4759123141LL) return miller_rabin(n, {2, 7, 61});
-    else return miller_rabin(n, {2, 325, 9375, 28178, 450775, 9780504, 1795265022});
-}
